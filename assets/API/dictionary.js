89126f0a788039a06436c1c8446e8742dd26cb76
justinbesteman <justin@besteman.io>
2016-11-13 21:36:26 -0500

    Finished dictionary.js and the ajax and json

---
+++ assets/API/dictionary.js
@@ -0,0 +1,48 @@
+// https://www.wordsapi.com/
+
+var word = "dance";
+
+
+$.ajax({
+    url: "https://wordsapiv1.p.mashape.com/words/last/similarTo", // The URL to the API. You can get this in the API page of the API you intend to consume
+    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+    data: {}, // Additional parameters here
+    dataType: 'json',
+    success: function(data) { console.dir((data.source)); console.log(data);},
+    error: function(err) { alert(err); },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", "JVsFpSsea5mshtsH7N5dZQOYQd0yp1dqScujsnjdKNIoipqLfS"); // Enter here your Mashape key
+    }
+
+
+}).done(function(response){
+
+
+	console.log(response);
+
+
+});
+
+
+/*{
+  "word": "bloody",
+  "similarTo": [
+    "unmitigated",
+    "butcherly",
+    "crimson",
+    "gory",
+    "homicidal",
+    "internecine",
+    "murderous",
+    "red",
+    "sanguinary",
+    "sanguineous",
+    "slaughterous",
+    "violent",
+    "blood-filled",
+    "bloodstained",
+    "bloodsucking",
+    "bloodthirsty",
+    "bloody-minded"
+  ]
+}*/
