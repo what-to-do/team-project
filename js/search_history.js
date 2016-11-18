c4b40b8262caa75d4e46a43bca52d8fbeb25b1fc
Jeff Swearingen <jeffswearingen@gmail.com>
2016-11-18 15:33:03 -0500

    search history can store new items and store to local storage

--- js/search_history.js
+++ js/search_history.js
@@ -35,3 +35,39 @@ function search_history() {
 	event_listener();
 
 } // End of search_history function
+
+function search_history_add(baton, search_history_record) {
+	// this function takes the baton,
+	// searches the search history for it
+	// if not present, the baton is pushed to the search history
+	// then the search history is saved in local storage
+
+	var found = false;
+
+	for (var i = 0; i < search_history_record.length; i++) {
+
+		if (baton === search_history_record[i]) {
+
+			found = true;
+
+		}
+
+	}
+
+	if (found === false) {
+
+		search_history_record.push(baton);
+
+		localStorage["search_history_record"] = JSON.stringify(search_history_record);
+
+		console.log(search_history_record);
+
+	}
+}
+
+function search_history_retrieve(search_history_record) {
+	// this function retrieves the search history record from local storage
+
+	search_history_record = JSON.parse(localStorage["search_history_record"]);
+
+}
