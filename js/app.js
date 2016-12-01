e5fc9fdf528c3fedd3fbf85b19ad57865eea4724
Jeff Swearingen <jeffswearingen@gmail.com>
2016-11-30 21:14:30 -0500

    fixed local storage issue for search history

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
 //google_image();
 
 search_history_record = new Array();
+search_history_retrieve();
+search_history();
 create_sidebar_btn();
 welcome_splash();
 event_listener();
